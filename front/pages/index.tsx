2b6ed854967fc27eb56c6b30f95450bf1d7f4c6a
Tristan <tristan.faraci@uha.fr>
2022-12-01 22:43:44 +0100

    feat(navBar.tsx): Création component NavBar

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -1,11 +1,15 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import NavBar from '../components/navBar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-     <p>home page</p>
+    <div>
+      <NavBar/>
+      <div className={styles.container}>
+        <p>home page</p>
+      </div>
     </div>
   )
 }
