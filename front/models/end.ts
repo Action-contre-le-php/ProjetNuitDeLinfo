bc3d83af3d87e9d91db62acca46631cb61dbe057
noa.ostermann <ostermann.noa1226@gmail.com>
2022-12-02 03:22:34 +0100

    feat(end.ts): Creation classe End + Ajout DeadEnd

---
+++ front/models/end.ts
@@ -0,0 +1,11 @@
+export default class End {
+    Title: string = '';
+    Text: string = '';
+    Picture: string = '';
+
+    constructor(Title: string, Text: string, Picture: string) {
+        this.Title = Title;
+        this.Text = Text;
+        this.Picture = Picture;
+    }
+}
