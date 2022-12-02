8e2f63dfeb89ea7a45c393c53ae31c51ba32dfed
etienne.pimmel <etienne.pimmel@uha.fr>
2022-12-02 02:03:35 +0100

    feat: Add systeme of prerequisites for answers

--- front/service/moveToNextDialog.tsx
+++ front/service/moveToNextDialog.tsx
@@ -2,14 +2,25 @@ import User from "../models/user";
 import Dialog from "../models/dialog";
 import Answer from "../models/answer";
 
-export default function MoveToNextDialog (user: User, actualDialog: Dialog, answerChoose: Answer, allDialog: Dialog[]): Dialog {
+export default function MoveToNextDialog (user: User, actualDialog: Dialog, answerChoose: Answer, allDialogs: Dialog[], allAnswers: Answer[]): Dialog {
     user.lastDialogId = actualDialog.id;
     user.historyAnswersId.push(answerChoose.id);
     user.currentDialogId = answerChoose.dialogId;
-    let result: Dialog | undefined = allDialog.find(t => t.id == answerChoose.dialogId);
+    let result: Dialog | undefined = allDialogs.find(dialog => dialog.id == answerChoose.dialogId);
 
     if (result === undefined){
         throw new Error();
+    }
+
+    for(let i: number = 0; i < result.answersId.length; i++){
+        let answer: Answer | undefined = allAnswers.find(answer => answer.id == result!.answersId[i]);
+        if (answer !== undefined && answer.prerequisites.length > 0){
+            for (let j: number = 0; j < answer.prerequisites.length; j++) {
+                if(user.historyAnswersId.indexOf(answer.prerequisites[j]) == -1){
+                    result.answersId.slice(i, 1);
+                }
+            }
+        }
     }
 
     return result;
