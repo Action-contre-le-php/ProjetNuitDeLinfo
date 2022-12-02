1e51ed29fbea7dd182b661655d4ccba254035aa0
Titouan FUCHS <titouan.fuchs@gmail.com>
2022-12-02 01:33:54 +0100

    feat: displaying links and answers in game page

--- front/pages/game/index.tsx
+++ front/pages/game/index.tsx
@@ -2,37 +2,52 @@ import {NextPage} from "next";
 import {Card, CardBody, CardFooter, Typography} from "@material-tailwind/react";
 import WikilinkComponent from "./components/WikilinkComponent";
 import AnswerComponent from "./components/AnswerComponent";
-import {Answer} from "../../models/answer";
+import Answer from "../../models/answer";
+import {useEffect, useState} from "react";
+import Dialog from "../../models/dialog";
+import narrationService from "../../service/narrationService";
 
 const GamePage : NextPage = () => {
 
-    const testAnswer : Answer = new Answer(0, "Test answer", [0], 0);
+    let [currentDialog, setCurrentDialog] = useState<Dialog>();
+    let [answers, setAnswers] = useState<Answer[]>([]);
+
+    useEffect(() => {
+        let firstDialog : Dialog|undefined = narrationService.GetDialog(0);
+
+        if (firstDialog === undefined){
+            throw new Error("No dialog with id 0");
+        }
+
+        setCurrentDialog(firstDialog);
+        setAnswers(narrationService.GetDialogAnswers(firstDialog.id));
+    }, []);
+
+    if (currentDialog === undefined){
+        return <div></div>
+    }
 
     return (
         <div className={"flex flex-col space-y-10 text-center overflow-auto"}>
             <Card id={"DialogCard"} className={"h-96 flex flex-col m-5"}>
                 <CardBody className={"flex-grow"}>
                     <Typography>
-                        Dialog Text Here
+                        {currentDialog?.contents}
                     </Typography>
                 </CardBody>
                 <CardFooter divider className="flex justify-between py-3">
-                    <WikilinkComponent title={"Test"} link={"test"}/>
+                    {currentDialog!.links.map((link, index) => {
+                        return <WikilinkComponent title={link.name} link={link.url} key={index}/>
+                    })}
                 </CardFooter>
             </Card>
-
             <div className={"flex flex-col md:flex-row md:overflow-hidden md:flex-wrap md:justify-center"}>
-                <AnswerComponent answer={testAnswer}/>
-                <AnswerComponent answer={testAnswer}/>
-                <AnswerComponent answer={testAnswer}/>
-                <AnswerComponent answer={testAnswer}/>
-                <AnswerComponent answer={testAnswer}/>
-                <AnswerComponent answer={testAnswer}/>
-                <AnswerComponent answer={testAnswer}/>
-                <AnswerComponent answer={testAnswer}/>
+                {answers.map((answer, index) => {
+                    return <AnswerComponent answer={answer} key={index}/>
+                })}
             </div>
         </div>
-    )
+    );
 }
 
 export default GamePage;
---
+++ front/service/narrationService.ts
@@ -0,0 +1,30 @@
+import Dialog from "../models/dialog";
+import Answer from "../models/answer";
+
+let dialogs : Dialog[] = require('../test/dialogs.json')['dialogs'];
+let answers : Answer[] = require('../test/anwers.json')['answers'];
+
+function GetDialog(id:number) : Dialog | undefined {
+    console.log(dialogs);
+    return dialogs.find(d => d.id == id);
+}
+
+function GetAnswer(id:number) : Answer | undefined {
+    return answers.find(a => a.id == id);
+}
+
+function GetDialogAnswers(dialogId:number) : Answer[] {
+    let ids: number[] = dialogs.find(d => d.id == dialogId)?.answersId || [];
+    let result: Answer[] = [];
+
+    for (let i = 0; i < ids.length; i++) {
+        let answer = GetAnswer(ids[i]);
+        if (answer) {
+            result.push(answer);
+        }
+    }
+
+    return result;
+}
+
+export default {GetDialog, GetAnswer, GetDialogAnswers};
