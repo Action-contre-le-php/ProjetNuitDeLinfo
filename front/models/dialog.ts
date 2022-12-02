fc12b00a46e45c76a2ac56313d411506d24d423b
Titouan FUCHS <titouan.fuchs@gmail.com>
2022-12-02 01:33:12 +0100

    refactor(Dialog): replaced link: string[] by links: Link[]

--- front/models/dialog.ts
+++ front/models/dialog.ts
@@ -1,17 +1,18 @@
 import Answer from "./answer";
+import Link from "./link";
 
 export default class Dialog {
     id: number;
     title: string;
     contents: string;
     answersId: number[];
-    link: string[]
+    links: Link[]
 
-    constructor(Id: number, Title: string, Contents: string, AnswersId: number[], Link: string[]) {
+    constructor(Id: number, Title: string, Contents: string, AnswersId: number[], Link: Link[]) {
         this.id = Id
         this.title = Title;
         this.contents = Contents;
         this.answersId = AnswersId;
-        this.link = Link;
+        this.links = Link;
     }
 }
