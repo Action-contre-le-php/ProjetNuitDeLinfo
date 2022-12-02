2d170199ace400325a7d5808767f2408d94b5a19
noa.ostermann <ostermann.noa1226@gmail.com>
2022-12-02 05:26:50 +0100

    fix(footer.tsx): fix css

--- front/components/footer.tsx
+++ front/components/footer.tsx
@@ -35,29 +35,29 @@ export default function Footer() {
         </DialogFooter>
       </Dialog>
     </Fragment>
-        <div  className="flex flex-row sticky bottom-0 w-full  flew-nowrap h-40 bg-[#fecdd3] 2xl:h-auto">
-            <div className="w-3/12 mt-4 2xl:mt-4">
+        <div className="flex flex-row sticky bottom-0 w-full  flew-nowrap h-auto bg-[#fecdd3] 2xl:h-auto">
+            <div className="w-3/12 m-4 2xl:m-4">
                 <p className="text-center">Liens utiles :</p><br/>
                 <a target="_blank" href='https://www.sida-info-service.org'><p className="text-center text-blue-600 underline-offset-1">Sida info service</p></a>
                 <a target="_blank" href='https://www.hepatites-info-service.org'><p className="text-center text-blue-600 underline-offset-1">Hepatites info service</p></a>
                 <a target="_blank" href='https://www.vih-info-soignants.fr'><p className="text-center text-blue-600 underline-offset-1">Vih infos soignats</p></a>
-                <a target="_blank" href='https://www.ligneazur.org'><p className="text-center text-blue-600 underline-offset-1">Ligneazur</p></a>
+                <a target="_blank" href='https://www.ligneazur.org'><p className="text-center text-blue-600 ">Ligneazur</p></a>
             </div>
-                <div className="w-3/12 mt-10 2xl:mt-10">
+                <div className="w-3/12 m-10 2xl:m-10">
                     <Link href="/credits">
                     <p className="text-center">Crédits</p><br></br>
                     </Link>
                     <p className="text-center" onClick={handleOpen}>By Action contre le PHP Corporation</p>
                 </div>
                 <div className="flex flex-col w-3/12">
-                    <div className="self-center mt-14 2xl:mt-18">
+                    <div className="self-center m-14 2xl:m-18">
                         <div>
                             <Image src="/logo-uha.png" alt="logo" width={200} height={200}/>
                         </div>
                     </div>    
                 </div>
                 <div className="flex flex-col w-3/12">
-                    <div className="self-center mt-2 2xl:mt-7">
+                    <div className="self-center m-2 2xl:m-2">
                         <div>
                             <Image src="/uha4.0-logo.png" alt="logo" width={100} height={100}/>
                         </div>
