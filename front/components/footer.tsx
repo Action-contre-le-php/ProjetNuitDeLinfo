f6a20314d39168a2d529dcabf35dee346af35c77
noa.ostermann <ostermann.noa1226@gmail.com>
2022-12-02 04:03:20 +0100

    feat(footer.tsx): RickrollEE

--- front/components/footer.tsx
+++ front/components/footer.tsx
@@ -1,15 +1,46 @@
 import Image from "next/image"
 import Link from "next/link"
+import { Fragment, useState } from "react";
+import {
+  Button,
+  Dialog,
+  DialogHeader,
+  DialogBody,
+  DialogFooter,
+} from "@material-tailwind/react";
 
 export default function Footer() {
+    const [open, setOpen] = useState(false);
+ 
+  const handleOpen = () => setOpen(!open);
     return (
       <div>
+        <Fragment>
+      <Dialog open={open} handler={handleOpen}>
+        <DialogHeader>Êtes vous sûr de vouloir continuer ?</DialogHeader>
+        <DialogFooter>
+          <Button
+            variant="text"
+            color="red"
+            onClick={handleOpen}
+            className="mr-1"
+          >
+            <span>Non</span>
+          </Button>
+          <a target="_blank" href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'>
+          <Button variant="gradient" color="green" onClick={handleOpen}>
+            <span>Oui</span>
+          </Button>
+          </a>
+        </DialogFooter>
+      </Dialog>
+    </Fragment>
         <div style={{ position: "static", bottom: 0, width:"100%", height:'100%' }} className="flex flex-row flew-nowrap bg-[#fecdd3]">
                 <div className="w-4/12 mt-10">
                     <Link href="/credits">
                     <p className="text-center">Crédits</p><br></br>
                     </Link>
-                    <p className="text-center">By Action contre le PHP Corporation</p>
+                    <p className="text-center" onClick={handleOpen}>By Action contre le PHP Corporation</p>
                 </div>
                 <div className="flex flex-col w-4/12">
                     <div className="self-center mt-14">
